9a2b4044985685215ee2464f2e81d287e6d8c56a
Sreesha-R <80239409+Sreesha-R@users.noreply.github.com>
2022-03-31 20:44:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,65 +1,46 @@
-var last_position_of_x, last_position_of_y;
-color = "black";
-width_of_line = 2;
 
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
 
-var width = screen.width;
+color = "red"; 
 
-new_width =  screen.width - 70; 
-new_height = screen.height - 300;
-	if(width < 992)
-	{
-	document.getElementById("myCanvas").width = new_width;
-    document.getElementById("myCanvas").height = new_height;
-    document.body.style.overflow = "hidden";
-	}
-	canvas.addEventListener("touchstart", my_touchstart);
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(100,100,50 ,0, 2*Math.PI);
+ctx.stroke();
 
-function my_touchstart(e) 
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
 {
-	console.log("my_touchstart");
-  //Additional Activity
-  color = document.getElementById("color").value;
-  width_of_line = document.getElementById("width_of_line").value;
-  //End Additional Activity
-         
-    last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-    last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-}
-
-canvas.addEventListener("touchmove", my_touchmove);
-
-function my_touchmove(e) 
-{
-
-	console.log("my_touchMove");
-    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
-    //  same as the paint web app
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
-
-    console.log("Last position of x and y coordinates = ");
-    console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-    ctx.moveTo(last_position_of_x, last_position_of_y);
-
-    console.log("Current position of x and y coordinates = ");
-    console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-    ctx.stroke();
-
-    last_position_of_x = current_position_of_touch_x; 
-    last_position_of_y = current_position_of_touch_y;
+    //taking color from input box
+    //additional activity start
+    color = document.getElementById("color").value;
+    console.log(color);
+    //addition activity ends
     
-    // end same as the paint web app
+     mouse_x = e.clientX - canvas.offsetLeft;
+     mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
+    circle(mouse_x , mouse_y);    
 }
 
-    // old same old as the paint web app
-function clearArea() {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+function circle(mouse_x , mouse_y)
+{
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
 }
-    // end of old same old as the paint web app
+
+//additional activity
+
+function clearArea()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+	
